f8871a3ff8b750aa68c366747acd7944c0051364
Stênio <gobis@Mac-mini-de-Stenio.local>
2022-09-14 19:14:03 -0300

    Criei um arquivo conversor de unidades

---
+++ ex008.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+n = float(input('Insira um valor em metros: '))
+print('O valor {}m em centímetros é igual a {:.1}'.format(n, n * 100))
+print('O valor {}m em milímetros é igual a {:.1}'.format(n, n * 1000))
